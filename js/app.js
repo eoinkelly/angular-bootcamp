82f5b12801e78c9acd7cf32c94f96e98b8ae1055
Eoin Kelly <eoin@rabidtech.co.nz>
2014-02-15 00:05:19 +1300

    More work on Restangular

--- js/app.js
+++ js/app.js
@@ -8,13 +8,15 @@ angular.module('App', ['filters', 'controllers', 'directives', 'ngRoute', 'resta
   
   // configure Restangular service
   .config(function(RestangularProvider) {
-    RestangularProvider.setBaseUrl('/api/v1');
+    // RestangularProvider.setBaseUrl('http://angular-bootcamp-demo.herokuapp.com');
+    RestangularProvider.setBaseUrl('http://localhost:3000');
+    RestangularProvider.setRequestSuffix('.json');
 
     // This is where you can hook into the requests and responses sent/recieved
     // by Restangular to massage them into what you want for your app.
     // Restangular tutorial: http://www.ng-newsletter.com/posts/restangular.html
     // Restangular docs: https://github.com/mgonto/restangular
-    
+
     // can add element transformations here to add custom methods to Restangular objects
 
     // can add request interceptors here to translate the response we get from the server
@@ -26,6 +28,33 @@ angular.module('App', ['filters', 'controllers', 'directives', 'ngRoute', 'resta
     // can add custom fields - this lets you map attributes on the backend to
     // different attributes in the Restangular objects in memory
   })
+
+  // Rectangular setup for MongoDB
+  // =============================
+  // TODO: 
+  // * change api key to your own
+  // * change database name from 'startup-wknd' to your own
+  // .constant('apiKey', 'your api key here')
+  //
+  // .config(function (RestangularProvider, apiKey) {
+  //   RestangularProvider.setBaseUrl('https://api.mongolab.com/api/1/databases/startup-wknd');
+  //   RestangularProvider.setDefaultRequestParams({
+  //     apiKey: apiKey
+  //   });
+  //   RestangularProvider.setRestangularFields({
+  //     id: '_id.$oid'
+  //   });
+  //
+  //   RestangularProvider.setRequestInterceptor(
+  //     function (elem, operation) {
+  //
+  //       if (operation === 'put') {
+  //         elem._id = undefined;
+  //         return elem;
+  //       }
+  //       return elem;
+  //     });
+  // })
 
   // configure $compile service
   .config(function ($compileProvider) {
--- js/controllers/rest-test.js
+++ js/controllers/rest-test.js
@@ -1,6 +1,38 @@
 'use strict';
 
 angular.module('rest-test-ctrl', ['restangular'])
-  .controller('RestTestCtrl', function ($scope, $log, Restangular) {
+  .controller('RestTestCtrl', function ($scope, $log, Restangular, $http) {
     $log.log('In RestTestCtrl()');
+
+    // var userToAdd = {
+    //   name:'AngularJS Superhero',
+    //   email:'superhero@angularjs.org'
+    // };
+
+    // $http example
+
+    // Get list of databases from MongoDB
+    // $http.get('https://api.mongolab.com/api/1/databases',
+    //   {
+    //     params: { apiKey:'AZoqYGFFD8zN7IGaOHTeZ2QrbKpysm2e' }
+    //   }
+    // )
+    // .success(function (data, status, headers, config) {
+    //   $log.log('in success handler');
+    //   $log.log(data);
+    // })
+    // .error(function (data, status, headers, config) {
+    //   $log.log('in error handler');
+    // });
+
+    // Restangular example
+    // ===================
+    var posts = Restangular.all('posts'); // calling getList on this will issue GET /databases
+    posts.getList().then(function (posts) {
+      $log.log(posts);
+    });
+
+    Restangular.one('posts', 1).get().then(function (post) {
+      $log.log(post);
+    });
   });
